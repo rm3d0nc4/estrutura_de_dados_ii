dcadb854c92f51de68dd1cbe459b8c3bab40a65a
Romero Antonio Ramos de Mendonça <romeromendonca22@gmail.com>
2023-03-10 16:02:11 -0300

    Added exercise 3 and 4: Questao 13.6 and Questao 13.7

---
+++ 03_atividade_03/13_7.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include "../02_atividade_02/arvore.hpp"
+
+using namespace std;
+
+int igual(Arvore A, Arvore B) {
+    if(A == B) return 1;
+    else if(A != B) return 0;
+
+    if(A->item != B->item) return 0;
+    int esq = igual(A->esquerda, B->esquerda);
+    int dir = igual(A->direita, B->direita);
+    if(esq + dir == 2) return 1;
+    return 0;
+
+}
+
+int main() {
+    Arvore arvore1 = criarArvore(criarArvore(NULL, 5, NULL), 5, criarArvore(NULL, 5, NULL));
+    Arvore arvore2 = criarArvore(criarArvore(NULL, 5, NULL), 5, criarArvore(NULL, 5, NULL));
+    int arvoresIguais = igual(arvore1, arvore2);
+
+    if(arvoresIguais) {
+        cout << "Arvores iguais" << endl;
+    } else {
+        cout << "Arvores diferentes" << endl;
+    }
+}
